87d0adc3ac1ec390099d180d5798022e15599807
Frolova Victory <frolova.victory@gmail.com>
2015-11-26 17:57:18 +0300

    week 2 mongo db java driver

--- src/main/java/com/mongodb/App.java
+++ src/main/java/com/mongodb/App.java
@@ -9,6 +9,8 @@ public class App
     //first_commit
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        //System.out.println( "Hello World!" );
+
+
     }
 }
